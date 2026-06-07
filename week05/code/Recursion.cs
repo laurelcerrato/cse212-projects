2f5a5e0363f1336a130671e3aa7a69a6d1673487
laurelcerrato <lcerratoramirez@byupathway.edu>
2026-06-06 20:43:40 -0600

    put origninal code

--- week05/code/Recursion.cs
+++ week05/code/Recursion.cs
@@ -15,7 +15,13 @@ public static class Recursion
     public static int SumSquaresRecursive(int n)
     {
         // TODO Start Problem 1
-        return 0;
+        if (n <= 0)
+            return 0;
+        else
+        {
+            return n * n + SumSquaresRecursive(n - 1);
+        }
+
     }
 
     /// <summary>
@@ -40,6 +46,18 @@ public static class Recursion
     public static void PermutationsChoose(List<string> results, string letters, int size, string word = "")
     {
         // TODO Start Problem 2
+        if (word.Length == size)
+        {
+            results.Add(word);
+            return;
+        }
+        for (int i = 0; i < letters.Length; i++)
+        {
+            if (!word.Contains(letters[i]))
+            {
+                PermutationsChoose(results, letters, size, word + letters[i]);
+            }
+        }
     }
 
     /// <summary>
@@ -129,10 +147,11 @@ public static class Recursion
     {
         // If this is the first time running the function, then we need
         // to initialize the currPath list.
-        if (currPath == null) {
+        if (currPath == null)
+        {
             currPath = new List<ValueTuple<int, int>>();
         }
-        
+
         // currPath.Add((1,2)); // Use this syntax to add to the current path
 
         // TODO Start Problem 5
